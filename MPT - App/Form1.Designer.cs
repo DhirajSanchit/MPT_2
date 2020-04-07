8e97d3e5361e197eb980ebb684dc10c3235a8935
Amit <Dhirajsanchit@gmail.com>
2020-04-07 15:44:49 +0200

    Added UI elements
    UI Elements update

--- MPT - App/Form1.Designer.cs
+++ MPT - App/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Profielen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Profielen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(345, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "FAQ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Profielen
+            // 
+            this.Profielen.Location = new System.Drawing.Point(345, 153);
+            this.Profielen.Name = "Profielen";
+            this.Profielen.Size = new System.Drawing.Size(167, 69);
+            this.Profielen.TabIndex = 2;
+            this.Profielen.Text = "Speel";
+            this.Profielen.UseVisualStyleBackColor = true;
+            this.Profielen.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(321, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 61);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welkom!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(871, 532);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Profielen);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Profielen;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MPT - App/Form1.cs
+++ MPT - App/Form1.cs
@@ -16,5 +16,20 @@ namespace MPT___App
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
